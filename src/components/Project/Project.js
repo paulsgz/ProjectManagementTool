569330fc1bc666dd65cf01bcffbc5e28b6572f99
unknown <christian.seguiza@yahoo.com>
2023-04-07 10:46:57 -0400

    Documentation Updates

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,3 +1,8 @@
+/**
+This component is responsible for rendering a form to add new projects to a project management application.
+It uses React hooks to manage the state of the form inputs and fetches project data from a remote server using Axios.
+It also displays a notification message using the react-toastify library to inform users about the result of their actions.
+*/
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
@@ -5,24 +10,27 @@ import 'react-toastify/dist/ReactToastify.css';
 import "./Project.css";
 
 function AddProject() {
-  const [project, setProject] = useState("");
-  const [projectsList, setProjectsList] = useState([]);
+// State variables to manage the form inputs and the list of projects
+const [project, setProject] = useState("");
+const [projectsList, setProjectsList] = useState([]);
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response2 = await axios.get("https://pmtserver.onrender.com/projects");
-        setProjectsList(response2.data.map((project) => project));
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    fetchData();
-  }, [projectsList]);
+// Fetches the list of projects from the server and updates the state variable when projectsList changes
+useEffect(() => {
+async function fetchData() {
+try {
+const response2 = await axios.get("https://pmtserver.onrender.com/projects");
+setProjectsList(response2.data.map((project) => project));
+} catch (error) {
+console.error(error);
+}
+}
+fetchData();
+}, [projectsList]);
 
-  const sendData = async (e) => {
-    e.preventDefault();
-    const formData = new FormData();
+// Sends a POST request to create a new project when the form is submitted
+const sendData = async (e) => {
+e.preventDefault();
+const formData = new FormData();
   
     formData.append("project",project);
     const entries = formData.entries();
--- src/components/sideBar/sideBar.js
+++ src/components/sideBar/sideBar.js
@@ -5,27 +5,46 @@ import { AiFillFolderOpen } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
 import logo from "../../images/logo.png";
 
+/**
+ * A component that displays the sidebar of the application
+ * @param {Object} props - Component props
+ * @param {Object} props.user - The user object
+ * @param {Function} props.onAddTicketClick - The function to handle the "Add Ticket" button click event
+ * @param {Function} props.onCurrentTicketClick - The function to handle the "Current Tickets" button click event
+ * @param {Function} props.onDashboardClick - The function to handle the "Dashboard" button click event
+ * @param {Function} props.onNewProjectClick - The function to handle the "New Project" button click event
+ */
 function Sidebar({ user, onAddTicketClick, onCurrentTicketClick, onDashboardClick, onNewProjectClick }) {
+  // State hook for toggling the sidebar
   const [toggle, setToggle] = useState(false);
+  // Destructuring the name property from the user object
   const { Name } = user || {};
+  // The navigate function from the react-router-dom package
   const navigate = useNavigate();
 
+  /**
+   * Event handler for the "Sign Out" button click event
+   */
   const handleSignOutClick = () => {
     const signOut = window.confirm("Are you sure you want to sign out?");
     if (signOut) {
+      // Remove the user object from the session storage
       sessionStorage.removeItem("user");
+      // Navigate the user to the home page
       navigate("/");
     }
   };
 
   return (
     <div className="sidebar">
+      {/* The application logo and user name */}
       <div className="Sidelogo">
         <img src={logo} className="img-fluid" alt="logo" />
         <div className="profile">
           <h4>Hi {Name}</h4>
         </div>
       </div>
+      {/* The sidebar menu */}
       <ul className="menu">
         <li onClick={onDashboardClick}>
           <FaTachometerAlt />
@@ -43,7 +62,6 @@ function Sidebar({ user, onAddTicketClick, onCurrentTicketClick, onDashboardClic
           <FaPlus />
           <span>Add Ticket</span>
         </li>
-
         <li onClick={handleSignOutClick}>
           <FaSignOutAlt />
           <span>Sign Out</span>
