901c1c8524db2c02f6aca36e3a4ebbd458d48a8a
unknown <christian.seguiza@yahoo.com>
2023-04-08 14:09:33 -0400

    Documentation Updates

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
 import React, { useState, useEffect } from "react";
-import AddProject from "./components/Project/Project.js";
-import AddTicket from "./components/AddTicket/AddTicket.js";
-import CurrentTicket from "./components/CurrentTicket/CurrentTicket.js";
-import Sidebar from "./components/sideBar/sideBar.js";
-import Dashboard from "./components/Dashboard/Dashboard.js";
-import NavBar from "./components/navbar/navbar.js";
-import "bootstrap/dist/css/bootstrap.css";
-import { useNavigate } from "react-router-dom";
-import "./App.css";
+import AddProject from "./components/Project/Project.js"; // Import the AddProject component
+import AddTicket from "./components/AddTicket/AddTicket.js"; // Import the AddTicket component
+import CurrentTicket from "./components/CurrentTicket/CurrentTicket.js"; // Import the CurrentTicket component
+import Sidebar from "./components/sideBar/sideBar.js"; // Import the Sidebar component
+import Dashboard from "./components/Dashboard/Dashboard.js"; // Import the Dashboard component
+import NavBar from "./components/navbar/navbar.js"; // Import the NavBar component
+import "bootstrap/dist/css/bootstrap.css"; // Import Bootstrap CSS
+import { useNavigate } from "react-router-dom"; // Import the useNavigate hook from React Router
+import "./App.css"; // Import the custom CSS for the App component
 
 function App() {
-  const [showAddTicket, setShowAddTicket] = useState(false);
-  const [showCurrentTicket, setShowCurrentTicket] = useState(false);
-  const [showProject, setShowProject] = useState(false);
-  const [showAlert, setShowAlert] = useState(false);
-  const navigate = useNavigate();
-  const [sidebarVisible, setSidebarVisible] = useState(false);
+  const [showAddTicket, setShowAddTicket] = useState(false); // State to show or hide the AddTicket component
+  const [showCurrentTicket, setShowCurrentTicket] = useState(false); // State to show or hide the CurrentTicket component
+  const [showProject, setShowProject] = useState(false); // State to show or hide the AddProject component
+  const [showAlert, setShowAlert] = useState(false); // State to show or hide alerts
+  const navigate = useNavigate(); // The useNavigate hook allows us to navigate between routes programmatically
+  const [sidebarVisible, setSidebarVisible] = useState(false); // State to show or hide the Sidebar component
 
+  // This useEffect hook runs when the component mounts, and checks if the user is authenticated.
   useEffect(() => {
     const isAuthenticated = sessionStorage.getItem("user") !== null;
     if(!isAuthenticated){
       navigate("/");
     }
   }, []);
-  
 
-  const user = JSON.parse(sessionStorage.getItem("user")) || {};
-  const { Name, Email, Role } = user.user || {};
+  const user = JSON.parse(sessionStorage.getItem("user")) || {}; // Get the user object from session storage, or an empty object if it doesn't exist
+  const { Name, Email, Role } = user.user || {}; // Extract the Name, Email, and Role properties from the user object
 
-  console.log(user);
+  console.log(user); // Log the user object to the console for debugging purposes
 
+  // This function is called when a new ticket is created.
+  // It hides the AddTicket component, shows the CurrentTicket component, and reloads the window.
   const handleTicketCreated = () => {
     setShowAddTicket(false);
     setShowCurrentTicket(true);
     window.location.reload();
   };
 
+  // This function is called when the "Add Ticket" button is clicked.
+  // It shows the AddTicket component if the user has the "team leader" role, and hides the CurrentTicket and AddProject components.
+  // If the user doesn't have the "team leader" role, it shows an alert.
   const handleAddTicketClick = () => {
     if (Role !== "team leader") {
       alert("You do not have permission to add a ticket.");
@@ -46,12 +51,16 @@ function App() {
     }
   };
 
+  // This function is called when the "Current Ticket" button is clicked.
+  // It shows the CurrentTicket component and hides the AddTicket and AddProject components.
   const handleCurrentTicketClick = () => {
     setShowAddTicket(false);
     setShowCurrentTicket(true);
     setShowProject(false)
   };
 
+  // This function is called when the "Dashboard" button is clicked.
+  // It hides the AddTicket, CurrentTicket, and AddProject components.
   const handleDashboardClick = () => {
     setShowAddTicket(false);
     setShowCurrentTicket(false);
@@ -63,6 +72,7 @@ function App() {
     setShowAddTicket(false);
     setShowCurrentTicket(false);
   };
+  
   return (
     <div className="container-fluid">
       <NavBar
